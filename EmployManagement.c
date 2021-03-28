8348b74b72ac5b51b151ed2308aa15fbf27d6a2a
Hardik Gehlot <hardikgehlot2303@gmail.com>
2021-03-29 01:07:51 +0530

    created a function for auto generating id and update UI of show_data()

--- EmployManagement.c
+++ EmployManagement.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+int i;
 struct Employ_info{
   int id;
   char first_name[10];
@@ -10,8 +11,11 @@ FILE *fp;
 void add_data();
 void show_data();
 void welcome_screen();
+void getting_id();
 void main(){
+    i=0;
     welcome_screen();
+    getting_id();
     int option;
     while(option != 3){
         printf("\n\n>>>1.Add records\n>>>2.Show all records\n>>>3.Exit\n>>>Option: ");
@@ -45,8 +49,8 @@ void add_data(){
         printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
         printf("Enter Employ Details\n");
         printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
-        printf("ID: ");
-        scanf("%d",&e1.id);
+        printf("ID: %d\n",i+1);
+        e1.id=i+1;
         printf("FIRST NAME: ");
         scanf("%s",e1.first_name);
         printf("LAST NAME: ");
@@ -57,6 +61,7 @@ void add_data(){
         scanf("%f",&e1.salary);
         fwrite(&e1,sizeof(e1),1,fp);
         fclose(fp);
+        i++;
     }
 }
 void show_data(){
@@ -66,22 +71,39 @@ void show_data(){
         fclose(fp);
     }
     else{
-        printf("-------------------------------------------\n");
+        printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+        printf("Sr.No.   ID    First Name   Last Name   Position     Salary\n");
+        printf("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+        int j=1;
         while(fread(&e1,sizeof(e1),1,fp)){
-            printf("ID       : %d\n",e1.id);
-            printf("Name     : %s ",e1.first_name);
-            printf("%s\n",e1.last_name);
-            printf("Position : %s\n",e1.position);
-            printf("Salary   : %.2f\n",e1.salary);
-            printf("-------------------------------------------\n");
+            // printf("ID       : %d\n",e1.id);
+            // printf("Name     : %s ",e1.first_name);
+            // printf("%s\n",e1.last_name);
+            // printf("Position : %s\n",e1.position);
+            // printf("Salary   : %.2f\n",e1.salary);
+            printf("%-9d%-6d%-13s%-12s%-13s%.2f\n",j,e1.id,e1.first_name,e1.last_name,e1.position,e1.salary);
+            printf("---------------------------------------------------------------------\n");
+            j++;
         }
         fclose(fp);
         }
 }
 void welcome_screen(){
-     printf("\t\t\t################################################\n");
+    printf("\t\t\t################################################\n");
     printf("\t\t\t##                    EMPLOY                  ##\n");
     printf("\t\t\t##                  MANAGEMENT                ##\n");
     printf("\t\t\t##                    SYSTEM                  ##\n");
     printf("\t\t\t################################################");
+}
+void getting_id(){
+    fp = fopen("employ.dat","rb");
+    if(fp==NULL){
+        fclose(fp);
+    }
+    else{
+        while (fread(&e1,sizeof(e1),1,fp) == 1){
+            i=e1.id;
+        }
+        fclose(fp);
+    }
 }
